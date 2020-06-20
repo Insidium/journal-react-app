e2bfe446e53a198a72eff87d90f779203a6e9381
Insidium <ryanchallen@outlook.com>
2020-06-20 10:30:54 +1000

    Added home, category select and new entry components, added with routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import Home from './components/Home';
+import CategorySelection from './components/CategorySelection';
+import NewEntry from './components/NewEntry';
 
 const App = () => {
-  return (
-    <div >
-          The basic empty template
-    </div>
-  )
-}
+	return (
+		<div>
+			<h1>Journal App</h1>
+			<BrowserRouter>
+				<Route path='/' component={Home} />
+				<Route path='/category' component={CategorySelection} />
+				<Route path='/entry/new' component={NewEntry} />
+			</BrowserRouter>
+		</div>
+	);
+};
 
-export default App
+export default App;
---
+++ src/components/NewEntry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NewEntry = () => {
+	return (
+		<div>
+			<h1>New Entry</h1>
+		</div>
+	);
+};
+
+export default NewEntry;

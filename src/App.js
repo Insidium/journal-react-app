a891b5991d01a29c144a9462274c533fb53edb48
Insidium <ryanchallen@outlook.com>
2020-06-20 10:38:41 +1000

    Created not found component for wrong routes, added to app.js

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import CategorySelection from './components/CategorySelection';
 import NewEntry from './components/NewEntry';
+import NotFound from './components/NotFound';
 
 const App = () => {
 	return (
 		<div>
 			<h1>Journal App</h1>
 			<BrowserRouter>
-				<Route path='/' component={Home} />
-				<Route path='/category' component={CategorySelection} />
-				<Route path='/entry/new' component={NewEntry} />
+				<Route exact path='/' component={Home} />
+				<Route exact path='/category' component={CategorySelection} />
+				<Route exact path='/entry/new' component={NewEntry} />
+				<Route component={NotFound} />
 			</BrowserRouter>
 		</div>
 	);
